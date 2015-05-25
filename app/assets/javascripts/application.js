ad30cfb657f8da8b50f703832baae569c5388223
Jørgen Sundt <jorgen@theeleven.co.uk>
2015-05-25 18:27:08 +0100

    random animation tests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require typed
+//= require jqClock.min
 //= require_tree .
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,14 @@
 $(document).on('ready ajaxComplete', function() {
 
   $('.logo').addClass('drawing');
+  $('.watermark').addClass('drawing');
+  $('.content').addClass('drawing');
   $('.typed-cursor').addClass('drawing');
+
+
+  $(".js-clock").clock({
+    "calendar":"false",
+    "format":"24"
+  });
 
 });
